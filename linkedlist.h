baf8639905b7ef0fb058bf1866bb9dfcb6a92225
merry0414 <maruihong2006@163.com>
2026-03-25 13:00:12 +0800

    Add files via upload

---
+++ linkedlist.h
@@ -0,0 +1,15 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+typedef struct Node
+{
+	int val;
+	struct Node *next;
+}Node;
+Node *createnode(int data);
+void tail_append(Node **head, int data);
+void head_append(Node **head, int data);
+void traverse(Node *head);
+void delete(Node **head, int value);
+
+#endif
